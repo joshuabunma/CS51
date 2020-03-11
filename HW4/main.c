e14d4f5a0cd9e5602c3b41400c5a1002471298ce
joshuabunma <54158537+joshuabunma@users.noreply.github.com>
2020-03-11 14:22:25 -0700

    Add files via upload

---
+++ HW4/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node
+{
+    int goals;
+    struct node *next;
+} nodeType;
+
+int main()
+{
+
+
+    printf("Enter the number of goals allowed in the current game or -1 to quit");
+
+
+
+    return 0;
+}
